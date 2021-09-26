637f68a5dc3045481e1f0098b6f70cfb90661771
Nasirdin <zhaparkulov02@gmail.com>
2021-09-26 23:55:32 +0600

    add modal and sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 const burger = document.querySelector('.header__burger');
+const burgerModal = document.querySelector('.burgerModal');
 burger.addEventListener('click', function() {
     burger.classList.toggle('active')
+    burgerModal.classList.toggle('burgerActive')
 }) 
+
